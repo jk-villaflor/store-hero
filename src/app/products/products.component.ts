9e3f445ae7c344ea71c174fec573c7f08f11f62d
John-Kevin Villaflor <jvillaflor@myherohub.com>
2023-11-11 23:26:31 +0400

    added temporary data

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -17,13 +17,42 @@ import { ProductCardComponent } from '../product-card/product-card.component';
 export class ProductsComponent implements OnInit{
   
   productList : Product[] = [];
+  error: any;
+  sampleProduct = [
+    {
+      productId: 1,
+      productName: 'product 1',
+      productPrice: 111.99,
+      productImageLocation: '/product1.jpg'
+    },
+    {
+      productId: 2,
+      productName: 'product 2',
+      productPrice: 222.99,
+      productImageLocation: '/product2.jpg'
+    },
+    {
+      productId: 3,
+      productName: 'product 3',
+      productPrice: 3333.99,
+      productImageLocation: '/product3.jpg'
+    },
+  ]
   
   constructor(private productsService: ProductsService){}
 
   ngOnInit(): void {
+    // this.fetchData();
+  }
+
+  fetchData(): void {
     this.productsService.getProducts().subscribe( data =>{
       this.productList = data;
-      console.log('productList =>', this.productList);
-    });
+    },
+    (error) =>{
+      this.error = error;
+    }
+
+    );
   }
 }
